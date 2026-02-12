b9bb9319439ee3e4fb38de0042601a84ed4b0a13
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2026-02-12 14:05:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Bot Token from BotFather
-BOT_TOKEN = 'YOUR_BOT_TOKEN'
+BOT_TOKEN = '8463982454:AAFXhclFtn5cCoJLZl3l-SwhPMk3ssv6J8o'
 
 # You can add other configuration settings here as needed (e.g., API keys, DB settings)
