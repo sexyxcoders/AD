d2ff84055249f616e7fa7fa5540448bf0418298b
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2026-02-14 15:01:40 +0530

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,6 @@
 from .constants import CONFIG, BotConfig
-from .database import db, init_db, get_db_client
+from .database import init_db, get_db, get_db_client
+from .config import CONFIG
 from .bot import get_bot_config, API_ID, API_HASH, BOT_TOKEN, MONGO_URI
 
 __all__ = [
